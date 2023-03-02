ad376b5d8e10c6dbbd4c5749434c1088d38c9e17
kravetsd <kdykrg@gmail.com>
2023-03-02 12:30:30 +0300

    Exploring boltdb CRUD operations

--- main.go
+++ main.go
@@ -1,11 +1,70 @@
 package main
 
 import (
-	"github.com/kravetsd/task/cmd"
+	"fmt"
+	"log"
+	"sync"
+
+	"github.com/boltdb/bolt"
 )
+
+var (
+	db   *bolt.DB
+	once sync.Once
+)
+
+func GetDb() (*bolt.DB, error) {
+	once.Do(func() {
+		var err error
+		db, err = bolt.Open("my.db", 0600, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+		db.Update(func(tx *bolt.Tx) error {
+			_, err := tx.CreateBucketIfNotExists([]byte("Tasks"))
+			if err != nil {
+				return fmt.Errorf("create bucket: %s", err)
+			}
+			return nil
+		})
+	})
+	return db, nil
+}
 
 func main() {
 
-	cmd.Execute()
+	// Example update read-write transaction
+	db, err := GetDb()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db.Update(func(tx *bolt.Tx) error {
+		b, err := tx.CreateBucketIfNotExists([]byte("Tasks"))
+		if err != nil {
+			return err
+		}
+		return b.Put([]byte("1"), []byte("Task 1"))
+	})
+
+	// Example reading transaction from the bucket
+	db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("Tasks"))
+		v := b.Get([]byte("1"))
+		fmt.Println(string(v))
+		return nil
+	})
+
+	db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("Tasks"))
+		b.ForEach(func(k, v []byte) error {
+			fmt.Printf("key=%s, value=%s", k, v)
+			return nil
+		})
+		return nil
+	})
+
+	db.Close()
+	//cmd.Execute()
 
 }
